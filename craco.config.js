88c8dea63359a33c6e2db061c217bd605a43d018
havardmj <havard.myrvold.johannessen@nav.no>
2023-02-02 01:45:19 +0100

    app init

---
+++ craco.config.js
@@ -0,0 +1,44 @@
+const CracoLessPlugin = require('craco-less');
+const path = require('path');
+const { EnvironmentPlugin } = require('webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+
+module.exports = {
+    webpack: {
+        plugins: [
+            new BundleAnalyzerPlugin({
+                analyzerMode: 'disabled',
+                openAnalyzer: false,
+            }),
+            new EnvironmentPlugin({
+                GIT_COMMIT_HASH: 'local-dev',
+            }),
+        ],
+        alias: {
+            '@': path.resolve(__dirname, 'src/'),
+        },
+    },
+    plugins: [
+        { plugin: CracoLessPlugin },
+    ],
+    eslint: {
+        enable: true,
+        mode: 'extends',
+        configure: {
+            extends: 'react-app',
+            rules: {
+                // Det er en bug i denne sjekken som automatisk feiler på ÆØÅ:
+                // https://github.com/yannickcr/eslint-plugin-react/issues/1654
+                'react/jsx-pascal-case': 'off',
+            },
+        },
+    },
+    jest: {
+        configure: {
+            moduleNameMapper: {
+                '^@/(.*)$': '<rootDir>/src/$1',
+            },
+        },
+    },
+};
